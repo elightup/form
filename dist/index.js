0eb88becaa0d950c51fad42b2e111d7fc2f94c78
Anh Tran <anhtnt@elightup.com>
2022-01-26 17:03:54 +0700

    Fix export

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { Control } from "./controls/Control";
-export { Input } from "./controls/Input";
-export { Select } from "./controls/Select";
-export { Text } from "./controls/Text";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export { Control } from "./controls/Control";
-export { Input } from "./controls/Input";
-export { Select } from "./controls/Select";
-export { Text } from "./controls/Text";
+export { default as Control } from "./controls/Control";
+export { default as Input } from "./controls/Input";
+export { default as Select } from "./controls/Select";
+export { default as Text } from "./controls/Text";
 
