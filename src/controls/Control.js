5559cd3ee888880ad17feaa2fd807a2d08cccf0a
Anh Tran <anhtnt@elightup.com>
2022-02-20 10:34:54 +0700

    Revert "Don't render control div if no label and description"
    This reverts commit 11e0d6707ac8307d1b835deba9b4d236b5f8aac2.

--- src/controls/Control.js
+++ src/controls/Control.js
@@ -11,19 +11,21 @@ const Control = ( {
 	className = '',
 	children,
 	...rest
-} ) => !label && !description ? children : <div className={ clsx( 'ef-control', className ) } { ...rest }>
-	{
-		label &&
-		<label className="ef-control__label" htmlFor={ id }>
-			{ label.includes( '<' ) ? <RawHTML>{ label }</RawHTML> : label }
-			{ required && <span className="ef-control__required">*</span> }
-			{ tooltip && <Tooltip content={ tooltip } /> }
-		</label>
-	}
-	<div className="ef-control__input">
-		{ children }
-		{ description && <RawHTML className="ef-control__description">{ description }</RawHTML> }
+} ) => (
+	<div className={ clsx( 'ef-control', className ) } { ...rest }>
+		{
+			label &&
+			<label className="ef-control__label" htmlFor={ id }>
+				{ label.includes( '<' ) ? <RawHTML>{ label }</RawHTML> : label }
+				{ required && <span className="ef-control__required">*</span> }
+				{ tooltip && <Tooltip content={ tooltip } /> }
+			</label>
+		}
+		<div className="ef-control__input">
+			{ children }
+			{ description && <RawHTML className="ef-control__description">{ description }</RawHTML> }
+		</div>
 	</div>
-</div>;
+);
 
 export default Control;
