cc0a29ebf0fa463698d79a2423e4ce2028a0fc4b
Anh Tran <anhtnt@elightup.com>
2023-12-28 18:10:33 +0700

    Set tooltip delay = 0

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,7 +1,7 @@
 import { Dashicon, Tooltip as T } from "@wordpress/components";
 
 const Tooltip = ( { content } ) => (
-	<T text={ content }>
+	<T text={ content } delay={ 0 }>
 		<span className="ef-control__tooltip"><Dashicon icon="editor-help" /></span>
 	</T>
 );
